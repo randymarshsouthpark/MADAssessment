91e0b8f28682051405cd54975afc8703b1f14973
randymarshsouthpark <2cricg55@solent.ac.uk>
2017-04-27 11:04:58 +0100

    starting 2nd screen

--- app/src/main/java/com/example/a2cricg55/madassessment/POI.java
+++ app/src/main/java/com/example/a2cricg55/madassessment/POI.java
@@ -1,9 +1,15 @@
 package com.example.a2cricg55.madassessment;
 
 import android.app.Activity;
+import android.os.Bundle;
 
 /**
  * Created by 2cricg55 on 27/04/2017.
  */
 public class POI extends Activity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.pointofinterest);
+    }
 }
